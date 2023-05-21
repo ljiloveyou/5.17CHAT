975f235b0a5e5a0b2a9aa8585d37577ad65223ac
ljiloveyou <133562422+ljiloveyou@users.noreply.github.com>
2023-05-21 18:54:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "202专用 ChatGPT",
+  description: "202专属ChatAI",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "202专用 ChatGPT",
     statusBarStyle: "default",
   },
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
